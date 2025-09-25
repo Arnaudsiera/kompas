b4fa57f1c1595be872cd20928552579dd097becd
Arnaudsiera <arnaud.siera@icloud.com>
2025-09-25 13:25:37 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,47 @@
+let kompasrichting = 0
+basic.forever(function () {
+    kompasrichting = input.compassHeading()
+})
+basic.forever(function () {
+    if (kompasrichting >= 45 && kompasrichting < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (kompasrichting >= 135 && kompasrichting < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (kompasrichting >= 225 && kompasrichting < 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (kompasrichting >= 315 && kompasrichting <= 360) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (kompasrichting >= 0 && kompasrichting < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
